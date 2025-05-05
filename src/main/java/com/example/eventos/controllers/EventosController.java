bb8dae2e109a05c5c31b27006f420e7c3522eeb3
Thayrine Silva <thay.agueda@gmail.com>
2025-05-05 00:17:42 -0300

    Criando o crontroller de eventos

---
+++ src/main/java/com/example/eventos/controllers/EventosController.java
@@ -0,0 +1,13 @@
+package com.example.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventosController {
+	
+	@RequestMapping("/eventos/form")
+	public String form() {
+		return "formevento";
+	}
+}
