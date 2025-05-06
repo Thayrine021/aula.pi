b9496e6d479e3dbfc748b93492757985d6fde717
Thayrine Silva <thay.agueda@gmail.com>
2025-05-06 11:30:52 -0300

    Criando página de confirmação de evento após salvar dados

--- src/main/java/com/example/eventos/controllers/EventosController.java
+++ src/main/java/com/example/eventos/controllers/EventosController.java
@@ -1,13 +1,27 @@
 package com.example.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.eventos.models.Evento;
+
 @Controller
+@RequestMapping("/eventos")
 public class EventosController {
-	
-	@RequestMapping("/eventos/form")
-	public String form() {
-		return "formevento";
-	}
+
+    @GetMapping("/form")
+    public String form() {
+        return "formevento";
+    }
+
+    @PostMapping
+    public String salvar(Evento evento) {
+        System.out.println("Nome: " + evento.getNome());
+        System.out.println("Local: " + evento.getLocal());
+        System.out.println("Data: " + evento.getData());
+        System.out.println("Horário: " + evento.getHorario());
+        return "eventoConfirmado";
+    }
 }
