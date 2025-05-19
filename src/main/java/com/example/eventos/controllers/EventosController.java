131c9404df73fda45c6fd403c43913ff8069a170
Thayrine Silva <thay.agueda@gmail.com>
2025-05-19 01:13:47 -0300

    Criando listagem de eventos

--- src/main/java/com/example/eventos/controllers/EventosController.java
+++ src/main/java/com/example/eventos/controllers/EventosController.java
@@ -1,10 +1,13 @@
 package com.example.eventos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.eventos.models.Evento;
 import com.example.eventos.repositories.EventosRepository;
@@ -31,7 +34,10 @@ public class EventosController {
         return "eventos/eventoConfirmado";
     }
     @GetMapping
-    public String listar() {
-    	
+    public ModelAndView listar() {
+    	List<Evento> eventos = er.findAll();
+    	ModelAndView mv = new ModelAndView("eventos/lista");
+    	mv.addObject("eventos", eventos);
+    	return mv;
     }
 }
--- src/main/java/com/example/eventos/controllers/IndexController.java
+++ src/main/java/com/example/eventos/controllers/IndexController.java
@@ -8,7 +8,8 @@ public class IndexController {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "home";
+		System.out.println("Chamou o método index");
+		return "redirect:/eventos";
 	}
 
 }
