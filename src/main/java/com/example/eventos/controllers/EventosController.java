d96c878b9fa310a5a328f2b904e2e9533b9264e6
Thayrine Silva <thay.agueda@gmail.com>
2025-05-19 00:06:25 -0300

    Correções-aula 03

--- src/main/java/com/example/eventos/controllers/EventosController.java
+++ src/main/java/com/example/eventos/controllers/EventosController.java
@@ -1,15 +1,20 @@
 package com.example.eventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.eventos.models.Evento;
+import com.example.eventos.repositories.EventosRepository;
 
 @Controller
 @RequestMapping("/eventos")
 public class EventosController {
+	
+	@Autowired
+	private EventosRepository er;
 
     @GetMapping("/form")
     public String form() {
@@ -22,6 +27,11 @@ public class EventosController {
         System.out.println("Local: " + evento.getLocal());
         System.out.println("Data: " + evento.getData());
         System.out.println("Horário: " + evento.getHorario());
+        er.save(evento);
         return "eventos/eventoConfirmado";
+    }
+    @GetMapping
+    public String listar() {
+    	
     }
 }
